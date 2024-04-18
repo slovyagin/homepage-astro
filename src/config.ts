bd5c74959242f3b0c55ffc2cf02980a753980629
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-04-18 23:07:04 +0200

    Astro init

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  bio: "B. 1990, Ukraine",
+  description: `Selected pictures: 2015–${new Date().getFullYear()}`,
+  image:
+    "https://images.slovyagin.com/upload/v1630866054/homepage/191510530_lveoxs.jpg",
+  title: "Bogdan Slovyagin",
+  url: "https://www.slovyagin.com/",
+};
