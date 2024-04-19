48ea4199f67f7d806c524df72d768dda021ce13b
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-04-19 12:04:37 +0200

    Static rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
-  integrations: [vue()]
+  output: "static",
 });
