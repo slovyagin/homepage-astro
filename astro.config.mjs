55e2f027f8828b0ebb9acc848810e495bc9aa564
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-04-24 09:53:05 +0200

    mobile dvh view

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://slovyagin.com/",
   output: "static",
+  site: "https://slovyagin.com/",
 });
