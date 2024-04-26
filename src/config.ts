a1a7e4b10064095f9317867ca5178f2fa1bf1631
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-04-26 23:52:50 +0200

    - bio

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 export const config = {
-  bio: "B. 1990, Ukraine",
   description: `Selected pictures: 2015–${new Date().getFullYear()}`,
   image:
     "https://images.slovyagin.com/upload/v1630866054/homepage/191510530_lveoxs.jpg",
