807f730509aeec1d6221560ca38f328450eb446b
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-05-30 23:28:26 +0200

    Revert "SSR 2"
    This reverts commit 85d1e4a40d3781394cfb5a53bede5a76a22c9179.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: "https://slovyagin.com/",
   prefetch: true,
-  adapter: cloudflare(),
-  vite: {
-    ssr: {
-      external: [...builtinModules, ...builtinModules.map((m) => `node:${m}`)],
-      noExternal: process.env.NODE_ENV === "production" ? [/.*/] : undefined,
-    },
-  },
 });
