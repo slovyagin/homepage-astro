234f5a9bbb2fd20526124c42daed901ba8d84720
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-05-31 20:18:44 +0200

    Minor tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-import { builtinModules } from "node:module";
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
