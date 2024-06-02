ab0aea1e5201ca8f3b07e0d91bdbe07df542c70f
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-06-02 10:07:11 +0200

    kv bulk

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { createHash } from "node:crypto";
 import { defineMiddleware } from "astro:middleware";
 import { v2 as cloudinary } from "cloudinary";
 import type { Image } from "./types";
+import { invertColor } from "./middleware/invert-color";
+import { shuffle } from "./middleware/shuffle";
 
 let images: Array<Image> = [];
 const BASELINE_SIZE = 1400;
@@ -27,26 +29,30 @@ const CLOUDFLARE_RESOURCES_KV_BEARER =
   import.meta.env.CLOUDFLARE_RESOURCES_KV_BEARER ??
   process.env.CLOUDFLARE_RESOURCES_KV_BEARER;
 
-const kvHash = await fetch(
-  `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/values/${CLOUDFLARE_RESOURCES_HASH_KV_KEY_NAME}`,
-  {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${CLOUDFLARE_RESOURCES_KV_BEARER}`,
-    },
-  }
-).then((res) => res.text());
-const kvImages = await fetch(
-  `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/values/${CLOUDFLARE_RESOURCES_KV_KEY_NAME}`,
-  {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${CLOUDFLARE_RESOURCES_KV_BEARER}`,
-    },
-  }
-).then((res) => res.json());
+console.time("Reading Cloudflare KV...\n");
+const [kvHash, kvImages] = await Promise.all([
+  fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/values/${CLOUDFLARE_RESOURCES_HASH_KV_KEY_NAME}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${CLOUDFLARE_RESOURCES_KV_BEARER}`,
+      },
+    }
+  ).then((res) => res.text()),
+  fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/values/${CLOUDFLARE_RESOURCES_KV_KEY_NAME}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${CLOUDFLARE_RESOURCES_KV_BEARER}`,
+      },
+    }
+  ).then((res) => res.json()),
+]);
+console.timeEnd("Reading Cloudflare KV...\n");
 
 if (import.meta.env.MODE === "production") {
   cloudinary.config({
@@ -57,7 +63,7 @@ if (import.meta.env.MODE === "production") {
   });
 
   try {
-    console.log("Fetching images from Cloudinary...\n");
+    console.time("Fetching images from Cloudinary...\n");
     const { resources } = await cloudinary.api.resources({
       context: true,
       max_results: 500,
@@ -65,7 +71,7 @@ if (import.meta.env.MODE === "production") {
       prefix: "photos",
       type: "upload",
     });
-    console.log("\nImages fetched\n");
+    console.timeEnd("Fetching images from Cloudinary...\n");
 
     const hash = createHash("sha256");
     hash.update(JSON.stringify(resources));
@@ -75,7 +81,7 @@ if (import.meta.env.MODE === "production") {
       console.log("Cloudflare KV is up to date\n");
       images = kvImages;
     } else {
-      console.log("Applying Cloudinary transformations...\n");
+      console.time("Applying Cloudinary transformations...\n");
       for await (const item of resources) {
         const res = await cloudinary.api.resource(item.public_id, {
           colors: true,
@@ -117,105 +123,35 @@ if (import.meta.env.MODE === "production") {
 
         images.push(image);
       }
-      console.log("Transformations applied\n");
+      console.timeEnd("Applying Cloudinary transformations...\n");
 
-      console.log("Updating Cloudflare KV...\n");
+      console.time("Updating Cloudflare KV...\n");
       await fetch(
-        `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/values/${CLOUDFLARE_RESOURCES_HASH_KV_KEY_NAME}`,
+        `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/bulk`,
         {
           method: "PUT",
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${CLOUDFLARE_RESOURCES_KV_BEARER}`,
           },
-          body: digestedHash,
+          body: JSON.stringify([
+            {
+              [CLOUDFLARE_RESOURCES_HASH_KV_KEY_NAME]: digestedHash,
+              [CLOUDFLARE_RESOURCES_KV_KEY_NAME]: JSON.stringify(images),
+            },
+          ]),
         }
       );
-      await fetch(
-        `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_RESOURCES_KV_ID}/values/${CLOUDFLARE_RESOURCES_KV_KEY_NAME}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${CLOUDFLARE_RESOURCES_KV_BEARER}`,
-          },
-          body: JSON.stringify(images),
-        }
-      );
-      console.log("Cloudflare KV updated\n");
+      console.timeEnd("Updating Cloudflare KV...\n");
     }
   } catch (error) {
     console.error(error);
     process.exit(1);
   }
-
-  function rgbToHsl(r: number, g: number, b: number) {
-    const red = r / 255;
-    const green = g / 255;
-    const blue = b / 255;
-    const max = Math.max(red, green, blue);
-    const min = Math.min(red, green, blue);
-    let h: number;
-    let s: number;
-    const l = (max + min) / 2;
-
-    if (max === min) {
-      h = s = 0;
-    } else {
-      const d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-      switch (max) {
-        case red:
-          h = (green - blue) / d + (green < blue ? 6 : 0);
-          break;
-        case green:
-          h = (blue - red) / d + 2;
-          break;
-        case blue:
-          h = (red - green) / d + 4;
-          break;
-      }
-      // @ts-ignore
-      h = h / 6;
-    }
-
-    return [h, s, l];
-  }
-
-  function invertColor(hex: string) {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    if (!result) {
-      return false;
-    }
-
-    const r = Number.parseInt(result[1] ?? "0", 16);
-    const g = Number.parseInt(result[2] ?? "0", 16);
-    const b = Number.parseInt(result[3] ?? "0", 16);
-
-    return Number(rgbToHsl(r, g, b)[2]) > 0.5;
-  }
 } else {
   const { default: sampleImages } = await import("./sample-images.json");
 
   images = sampleImages as Array<Image>;
-}
-
-function shuffle<T>(array: T[]) {
-  let currentIndex = array.length;
-
-  while (currentIndex !== 0) {
-    const randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // @ts-ignore
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
 }
 
 const shuffled = shuffle(images);
