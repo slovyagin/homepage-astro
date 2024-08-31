d6531588ad1d53f604e96f1fba5cb1ec64f9ee71
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-08-31 22:16:45 +0200

    ff meta font

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,6 @@ export const config = {
   title: "Bogdan Slovyagin",
   url: "https://www.slovyagin.com/",
 };
+
+export const FF_META_PRO_URL =
+  "https://use.typekit.net/af/271842/0000000000000000000175cf/27/l?primer=a6f5ed45567faadc7e0db47bdc53158db04cfed654edd51683b1b64d3d3ffecb&fvd=n6&v=3";
