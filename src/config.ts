5b6782f5658c11bf6f253cfbf87896a50869366b
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-08-31 23:27:05 +0200

    layout upd

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  description: `Selected pictures: 2015–${new Date().getFullYear()}`,
+  description: `Selected Pictures: 2015–${new Date().getFullYear()}`,
   image:
     "https://images.slovyagin.com/upload/v1630866054/homepage/191510530_lveoxs.jpg",
   title: "Bogdan Slovyagin",
