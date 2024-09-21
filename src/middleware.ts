182f8c92778e96d01dae0a05a5ae16563eea1b42
Bogdan Slovyagin <bogdan@slovyagin.com>
2024-09-21 15:16:21 +0200

    X-API-Key

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,13 @@ import { defineMiddleware } from "astro:middleware";
 const IMAGES_API_URL = 'https://build.slovyagin.com';
 
 async function fetchImages() {
-  const response = await fetch(IMAGES_API_URL);
+  const response = await fetch(IMAGES_API_URL, {
+        method: 'GET',
+        headers: {
+          'X-API-Key': import.meta.env.API_SECRET_KEY,
+          'Content-Type': 'application/json'
+        }
+      });
   if (!response.ok) {
     throw new Error(`Failed to fetch images: ${response.status} ${response.statusText}`);
   }
@@ -16,7 +22,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
       context.locals.images = await fetchImages();
     } catch (error) {
       console.error('Error fetching images:', error);
-      // Optionally, set a default value or handle the error as needed
       context.locals.images = [];
     }
   }
