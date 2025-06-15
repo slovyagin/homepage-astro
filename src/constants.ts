552e353f562b854d18b3f38d44841eca750105d1
Bogdan Slovyagin <bogdan@slovyagin.com>
2025-06-15 19:52:02 +0200

    Font embedding setup Responsive layout

--- src/constants.ts
+++ src/constants.ts
@@ -8,5 +8,11 @@ export const PROFILE = {
 	url: "https://www.slovyagin.com/",
 } as const;
 export const INITIAL_IMAGES_COUNT = 3;
-export const FF_META_PRO_URL =
-	"https://use.typekit.net/af/271842/0000000000000000000175cf/27/l?primer=388f68b35a7cbf1ee3543172445c23e26935269fadd3b392a13ac7b2903677eb&fvd=n6&v=3";
+export const FF_META_PRO_URL = "https://use.typekit.net/af/271842/0000000000000000000175cf/27/l?primer=388f68b35a7cbf1ee3543172445c23e26935269fadd3b392a13ac7b2903677eb&fvd=n6&v=3";
+
+export const TYPEFACE = {
+	name: 'FF Meta Pro',
+	url: FF_META_PRO_URL,
+	weight: 500,
+	type: 'sans-serif',
+}
