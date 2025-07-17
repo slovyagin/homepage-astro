0add0d53b59e5ad3e07ede7aab5435f3354fb2eb
Bogdan Slovyagin <bogdan@slovyagin.com>
2025-07-17 19:18:15 +0200

    Mencken

--- src/constants.ts
+++ src/constants.ts
@@ -8,13 +8,12 @@ export const profile = {
 	url: "https://www.slovyagin.com/",
 } as const;
 export const INITIAL_IMAGES_COUNT = 3;
-export const FRANKLIN_GOTHIC = "https://use.typekit.net/af/4e755e/00000000000000007735bbcb/31/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3";
+export const MENCKEN = "https://use.typekit.net/af/062057/00000000000000007735b804/31/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3";
 
-const acumin = {
-	name: 'Franklin GothicCompressed',
-	fallback: 'Arial Narrow',
-	url: FRANKLIN_GOTHIC,
+export const typeface = {
+	name: 'Mencken',
+	// fallback: 'Arial Narrow',
+	url: MENCKEN,
 	weight: 400,
-	type: 'sans-serif',
+	type: 'serif',
 }
-export const typeface = acumin
