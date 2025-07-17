e262e78ce101e5a5f544a04d6168ffe1ef67f6f3
Bogdan Slovyagin <bogdan@slovyagin.com>
2025-07-17 15:41:05 +0200

    Franklin Gothic

--- src/constants.ts
+++ src/constants.ts
@@ -2,18 +2,19 @@ import {getCurrentYear} from "./utils";
 import socialImage from "./assets/slovyagin.jpg";
 
 export const profile = {
-	description: `Selected Pictures: 2015–${getCurrentYear()}`,
+	description: `Selected Pictures: 2015\u200a–\u200a${getCurrentYear()}`,
 	image: socialImage.src,
 	title: "Bogdan Slovyagin",
 	url: "https://www.slovyagin.com/",
 } as const;
 export const INITIAL_IMAGES_COUNT = 3;
-export const FF_META_PRO_URL = "https://use.typekit.net/af/271842/0000000000000000000175cf/27/l?primer=388f68b35a7cbf1ee3543172445c23e26935269fadd3b392a13ac7b2903677eb&fvd=n6&v=3";
+export const FRANKLIN_GOTHIC = "https://use.typekit.net/af/4e755e/00000000000000007735bbcb/31/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3";
 
-export const typeface = {
-	name: 'FF Meta Pro',
+const acumin = {
+	name: 'Franklin GothicCompressed',
 	fallback: 'Arial Narrow',
-	url: FF_META_PRO_URL,
-	weight: 500,
+	url: FRANKLIN_GOTHIC,
+	weight: 400,
 	type: 'sans-serif',
 }
+export const typeface = acumin
