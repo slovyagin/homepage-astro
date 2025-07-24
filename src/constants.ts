05f021b88043f950fc675b5978b65d248a70dcb2
Bogdan Slovyagin <bogdan@slovyagin.com>
2025-07-24 21:26:20 +0200

    Font upd

--- src/constants.ts
+++ src/constants.ts
@@ -8,12 +8,12 @@ export const profile = {
 	url: "https://www.slovyagin.com/",
 } as const;
 export const INITIAL_IMAGES_COUNT = 3;
-export const MENCKEN = "https://use.typekit.net/af/062057/00000000000000007735b804/31/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3";
+export const FONT_URL = "https://use.typekit.net/af/062057/00000000000000007735b804/31/l?primer=388f68b35a7cbf1ee3543172445c23e26935269fadd3b392a13ac7b2903677eb&fvd=n4&v=3";
 
 export const typeface = {
 	name: 'Mencken',
 	fallback: null, //'Arial Narrow'
-	url: MENCKEN,
+	url: FONT_URL,
 	weight: 400,
 	type: 'serif',
 }
